1e5515d5242c91ed240b491703409779f9be9216
takuto277 <takuto.72.soccer@gmail.com>
2024-03-22 01:33:57 +0900

    fix

--- EnglishWordLearningAI/EnglishWordLearningAI/ViewModels/InputWordViewModel.swift
+++ EnglishWordLearningAI/EnglishWordLearningAI/ViewModels/InputWordViewModel.swift
@@ -8,11 +8,13 @@
 import Foundation
 
 final class InputWordViewModel: ObservableObject {
+    var englishWord: String = ""
+    var japaneseWord: String = ""
     // TODO: 入力値が日本語なら、英語翻訳/英語なら、日本語翻訳してそれぞれの値を返す処理
     //TODO: 文章生成APIRepository呼び出し処理(文章生成を実行予定)
     //TODO: 文章選択画面へ渡す値の詰め込み処理
     func getNewWordData() -> NewWordData {
         //TODO: 保持した値を詰め込んで返す
-        return NewWordData(englishWord: "apple", japansesWord: "りんご", response: [SentenceResponse(englishSentence: "this is a apple.", japaneseSentence: "これはりんごです。")])
+        return NewWordData(englishWord: englishWord, japansesWord: japaneseWord, response: [SentenceResponse(englishSentence: "this is a apple.", japaneseSentence: "これはりんごです。")])
     }
 }
