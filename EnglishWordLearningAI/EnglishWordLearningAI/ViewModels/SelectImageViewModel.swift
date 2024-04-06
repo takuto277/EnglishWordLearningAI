4cc0f60544add4d7bfe3a49895b7f0e30dbbaca5
takuto277 <takuto.72.soccer@gmail.com>
2024-04-06 20:59:59 +0900

    add file and image

---
+++ EnglishWordLearningAI/EnglishWordLearningAI/ViewModels/SelectImageViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SelectImageViewModel.swift
+//  EnglishWordLearningAI
+//
+//  Created by 小野拓人 on 2024/04/06.
+//
+
+import Foundation
---
+++ EnglishWordLearningAI/EnglishWordLearningAI/Views/SelectImageView.swift
@@ -0,0 +1,18 @@
+//
+//  SelectImageView.swift
+//  EnglishWordLearningAI
+//
+//  Created by 小野拓人 on 2024/04/06.
+//
+
+import SwiftUI
+
+struct SelectImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SelectImageView()
+}
