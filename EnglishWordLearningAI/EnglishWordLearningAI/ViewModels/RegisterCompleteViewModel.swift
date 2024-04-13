ba36e5adb9f616fad90d0d6fe60e46b9c7457d1f
takuto277 <takuto.72.soccer@gmail.com>
2024-04-13 16:29:54 +0900

    create RegisterCompleteView/ViewModel

---
+++ EnglishWordLearningAI/EnglishWordLearningAI/ViewModels/RegisterCompleteViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  RegisterCompleteViewModel.swift
+//  EnglishWordLearningAI
+//
+//  Created by 小野拓人 on 2024/04/13.
+//
+
+import Foundation
---
+++ EnglishWordLearningAI/EnglishWordLearningAI/Views/RegisterCompleteView.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterCompleteView.swift
+//  EnglishWordLearningAI
+//
+//  Created by 小野拓人 on 2024/04/13.
+//
+
+import SwiftUI
+
+struct RegisterCompleteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegisterCompleteView()
+}
